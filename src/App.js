675fe14031500eaccf92e139b6ff3c2af619c447
AritraStark <58631915+AritraStark@users.noreply.github.com>
2020-09-03 12:48:16 +0530

    commit-3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
 
 function App() {
+  const business = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Bordertown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+  };
   return (
     <div class="App">
       <h1>ravenous</h1>
